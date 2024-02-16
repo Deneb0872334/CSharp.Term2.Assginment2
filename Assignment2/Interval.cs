8a83220d30c317c8c25639dcb6974f258d338b3a
Deneb0872334 <c0872334@mylambton.ca>
2024-02-15 21:57:09 -0500

    Initial commit
    Initial commit

---
+++ Assignment2/Interval.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+internal class Interval
+{
+    private static void Main(string[] args)
+    {
+        bool blnVerified = false;
+        Decimal decimalValue;
+        do
+        {
+            Console.Write("Enter a real number: ");
+            var userInput = Console.ReadLine();
+            if (Decimal.TryParse(userInput, out decimalValue))
+            {
+                blnVerified = true;
+            } else
+            {
+                Console.Write("Please enter a valid number.");
+            }
+        } while (!blnVerified);
+        
+        if (((decimalValue >= 1) && (decimalValue <= 3)) || ((decimalValue == -10) || (decimalValue == -2))) {
+            Console.WriteLine("{0} x does belongs to I", decimalValue);
+        } else
+        {
+            Console.WriteLine("{0} x does not belong to I", decimalValue);
+        }
+    }
+
+}
+
+
+
